1383c7f7f9bcc9c7185a3ff2a17bdd19e2e02bba
Yvin Molero <ymolero@ba-mac-001.local>
2021-01-26 12:07:26 -0300

    fix: add more config pre-commit and pre-push

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-      const x = "sadasd";
-    
-      return 'Hello World!';
-  }
+    getHello(): string {
+        const x = 'sadasd';
+
+        return 'Hello World!';
+    }
 }
